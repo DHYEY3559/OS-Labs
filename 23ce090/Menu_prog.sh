ff7d2d2b5d76b9131fd5a49aae28e6f71f53d9c4
Dhyey Pithadia <dhyeypithadia@gmail.com>
2025-02-13 11:41:13 +0530

    5th Commit

---
+++ 23ce090/Menu_prog.sh
@@ -0,0 +1,24 @@
+echo "Menu"
+echo "--------------------------------------------------------------"
+echo "a. Display calendar of current month."
+echo "b. Display today’s date and time."
+echo "c. Display usernames those are currently logged in the system."
+echo "d. Display your name at given x, y position."
+echo "e. Display your terminal number."
+echo "--------------------------------------------------------------"
+read choice
+case $choice in
+a) fcal ;;
+b) date ;;
+c) who ;;
+d)  echo "enter x pos"
+    read x
+    echo "enter y pos"
+    read y
+    tput cup $x $y
+    echo "Dhyey Pithadia"
+    tput sgr0;;
+e) tty ;;
+f) exit ;;
+*) echo "invalid"
+esac
