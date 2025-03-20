bf6eca0b897df8c59442f78cfbd7e055157c6a87
Dhyey Pithadia <dhyeypithadia@gmail.com>
2025-03-20 10:44:08 +0530

    9th Commit

---
+++ 23ce090/executable_file.sh
@@ -0,0 +1,13 @@
+echo Executable files
+files=$(find /mnt/c/Users/dhyey/OneDrive/Desktop/"OS labs"/23ce090/ -type f)
+echo $files
+echo
+
+echo List of Directories
+dir=$(ls -d )
+echo $dir
+echo
+
+echo List of zero sized files
+zero=$(find -size 0)
+echo $zero
